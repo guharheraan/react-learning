d956c96ddf6ab077197a9b27f8a758b171053c30
guharheraan <guhar.heraan@gmail.com>
2024-08-06 09:27:00 +0500

    setting up react project

---
+++ viteproject/src/App.jsx
@@ -0,0 +1,12 @@
+import Ghk from "./ghk";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Guhar Heraan</h1>
+      <Ghk />
+    </div>
+  );
+}
+
+export default App;
---
+++ viteproject/src/ghk.jsx
@@ -0,0 +1,7 @@
+function Ghk(){
+    return(
+        <h2>i am a student</h2>
+    )
+}
+
+export default Ghk
