aa4819c2173c6e2a52c1355e6bcaa5069160a51e
guharheraan <guhar.heraan@gmail.com>
2024-08-14 14:28:45 +0500

    first interaction with redux toolkit

---
+++ 11_reduxToolkit_Todo/src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import AddTodo from './components/addTodo'
+import Todos from './components/Todos'
+
+function App() {
+
+
+  return (
+    <>
+      <h1>redux tool learning</h1>
+      <AddTodo /> 
+      <Todos />
+    </>
+  )
+}
+
+export default App
